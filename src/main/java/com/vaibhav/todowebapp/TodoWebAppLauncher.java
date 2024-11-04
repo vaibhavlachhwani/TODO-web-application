78f8e70666b1d9726384a461edd461e67abb911b
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-11-04 18:38:30 +0530

    initialize project

---
+++ src/main/java/com/vaibhav/todowebapp/TodoWebAppLauncher.java
@@ -0,0 +1,13 @@
+package com.vaibhav.todowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWebAppLauncher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoWebAppLauncher.class, args);
+    }
+
+}
