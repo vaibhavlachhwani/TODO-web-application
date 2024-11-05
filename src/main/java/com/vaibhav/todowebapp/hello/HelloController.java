710266f439661c2c2de63336981d6db4513be7e4
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-11-05 13:55:17 +0530

    introduce logging

--- src/main/java/com/vaibhav/todowebapp/hello/HelloController.java
+++ src/main/java/com/vaibhav/todowebapp/hello/HelloController.java
@@ -1,7 +1,12 @@
 package com.vaibhav.todowebapp.hello;
 
+import org.apache.jasper.compiler.JspUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,15 +14,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping("/hello")
     @ResponseBody
     public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        logger.debug("***** Received GET request at /hello endpoint");
+        logger.debug("***** Request param : name = {}", name);
+
         return String.format("Hello, %s!", name);
     }
 
     @RequestMapping("/hello-html")
     @ResponseBody
     public String sayHelloHtml(@RequestParam(value = "name", defaultValue = "World") String name) {
+        logger.debug("***** Received GET request at /hello-html endpoint");
+        logger.debug("***** Request param : name = {}", name);
+
         String htmlContent = String.format(
                     """
                         <!DOCTYPE html>
@@ -36,7 +49,12 @@ public class HelloController {
     }
 
     @RequestMapping("/hello-jsp")
-    public String sayHelloJSP() {
+    public String sayHelloJSP(@RequestParam(name = "name", defaultValue = "World") String name, Model model) {
+//        System.out.println("Request param is " + name);
+        logger.debug("***** Received GET request at /hello-jsp endpoint");
+        logger.debug("***** Request param : name = {}", name);
+
+        model.addAttribute("name", name);
         return "hello";
     }
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/hello.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/hello.jsp
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Hello!</title>
+    <title>Hey, ${name}!</title>
 </head>
 <body style="background-color: darkblue">
-    <h1 style="color: ivory">Hello, World!</h1>
+    <h1 style="color: ivory">Have a nice day ahead, ${name}!</h1>
 </body>
 </html>
