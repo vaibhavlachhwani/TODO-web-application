8d71fc1b72dcd4a994923c67b148a2e88ba92ee3
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-11-06 00:28:05 +0530

    implement hardcoded authentication

--- src/main/java/com/vaibhav/todowebapp/login/LoginController.java
+++ src/main/java/com/vaibhav/todowebapp/login/LoginController.java
@@ -1,5 +1,6 @@
 package com.vaibhav.todowebapp.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
-    private AuthenticationService authenticationService;
+    private final AuthenticationService authenticationService;
+
+    public LoginController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String serveLoginPage() {
@@ -20,6 +25,10 @@ public class LoginController {
     public String serveWelcomePage
             (@RequestParam(value = "username", required = true) String username,
              @RequestParam(value = "password", required = true) String password, ModelMap model) {
+        if (!authenticationService.authenticate(username, password)) {
+            return "login";
+        }
+
         model.put("username", username);
         model.put("password", password);
 
