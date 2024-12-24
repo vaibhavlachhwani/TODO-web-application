401480f4d9467aeaf968ec59267d85608ff7ce88
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2024-12-24 19:42:00 +0530

    prepare h2 database and configure Spring Security

--- src/main/java/com/vaibhav/todowebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/vaibhav/todowebapp/security/SpringSecurityConfiguration.java
@@ -3,11 +3,14 @@ package com.vaibhav.todowebapp.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -53,5 +56,18 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+
+        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+ 
+        return httpSecurity.build();
     }
 }
